f013896535b0a4ff9e71a748e28f475b52922136
Kevin Miller <kevin@driftabout.com>
2018-01-11 13:37:38 -0800

    requesthandlers console.logs working

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 
 const server = require('./server');
 const router = require('./router');
+const requestHandlers = require('./requestHandlers');
 
-server.start(router.route);
+const handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+
+server.start(router.route, handle);
+
--- router.js
+++ router.js
@@ -1,7 +1,9 @@
 'use strict';
 
-function route(pathname) {
+function route(handle, pathname) {
   console.log(`About to route a request for ${pathname}`);
+  if (typeof handle[pathname] !== 'function') return console.log(`No request handler found for ${pathname}`);
+  handle[pathname]();
 }
 
 exports.route = route;
