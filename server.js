fcd72fb1f396c136b5e0163581a1b15d1d8eb6ca
Kevin Miller <kevin@driftabout.com>
2018-01-11 15:32:39 -0800

    post data logging to console

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const url = require('url');
 
 function start(route, handle) {
   function onRequest(req, res){
+    let postData = '';
     let pathname = url.parse(req.url).pathname;
     console.log(`Request for ${pathname} received.`); 
 
-    route(handle, pathname, res);
+    req.setEncoding('utf8');
+
+    req.addListener('data', (postDataChunk) => {
+      postData += postDataChunk;
+      console.log(`Recieved POST data chunk "${postDataChunk}".`);
+    });
+
+    req.addListener('end', () =>{
+      route(handle, pathname, res, postData);
+    });
   }
 
   http.createServer(onRequest).listen(8888);
