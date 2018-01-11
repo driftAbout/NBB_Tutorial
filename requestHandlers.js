3224a9959c871c9f1d3b385c1bb01a8464ae6de0
Kevin Miller <kevin@driftabout.com>
2018-01-11 14:03:05 -0800

    10 sec block on start

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,8 +2,19 @@
 
 function start(){
   console.log('Request handler \'start\' was called.');
+  
+  function sleep(secs) {
+    let startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + (secs + 10000) );
+  }
+
+  sleep(10);
   return 'Hello Start';
+
 }
+
+
+
 
 function upload(){
   console.log('Request handler \'upload\' was called.');
